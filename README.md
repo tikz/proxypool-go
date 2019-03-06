f3d2580a0dda501b4ad7856bc56cb0e07e840be5
Tik <tik@lag.party>
2019-03-06 18:42:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# proxypool-go
+
+Proxypool is a wrapper around the h12.io/socks library that implements a pool of rotating SOCKS4/5 proxies for making HTTP requests. It is also safe to use with goroutines.
+
+
+# Example
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+	"proxypool"
+)
+
+func main() {
+	testURL := "http://example.com" // The URL used for testing all proxies (expects 200 code)
+	rateLimit := 10                 // How much seconds to wait between requests, per proxy
+	retestDelay := 300              // How much seconds to wait to retest a proxy that became down
+
+	pool := proxypool.NewProxyPool(testURL, rateLimit, retestDelay)
+
+	// Load proxies from file, expects ip:port per line
+	pool.LoadProxies("./proxies.txt")
+
+	// pool.Get() waits for a proxy from the pool to become available and retrying forever till it gets a 200 response
+	HTML := pool.Get("http://example.com/something")
+	fmt.Println(HTML)
+
+	// Also you can manually use:
+	proxy, err := pool.GetAvailableProxy()
+	if err != nil {
+		log.Fatal(err)
+	}
+	proxy.Get("http://example.com/something")
+}
+```
---
+++ proxypool.go
@@ -0,0 +1,167 @@
+package proxypool
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"h12.io/socks"
+)
+
+// Proxy represents a single SOCKS 4/5 proxy
+type Proxy struct {
+	URL         string
+	Alive       bool
+	protocol    string
+	ip          string
+	port        int
+	lastRequest time.Time
+	dial        func(string, string) (net.Conn, error)
+	transport   *http.Transport
+	client      *http.Client
+}
+
+// ProxyPool manages a group of proxies.
+// RateLimit is how many seconds to wait between requests, per proxy.
+// RetestDelay is after how many seconds should a proxy be retested if its is unavailable (manually call Pool.Test()).
+type ProxyPool struct {
+	proxies        []*Proxy
+	TestURL        string
+	RateLimit      int
+	RetestDelay    int
+	AliveCount     int
+	AvailableCount int
+}
+
+// Create checks if its v4 or v5, constructs the dial, transport, client and finally tests it.
+func (proxy *Proxy) Create(testURL string, wg *sync.WaitGroup) {
+	socksVersions := [2]int{5, 4}
+	for _, version := range socksVersions {
+		proxy.URL = fmt.Sprintf("socks%d://%s:%d", version, proxy.ip, proxy.port)
+		proxy.dial = socks.Dial(proxy.URL + "?timeout=10s")
+		proxy.transport = &http.Transport{Dial: proxy.dial}
+		proxy.client = &http.Client{Transport: proxy.transport, Timeout: 10 * time.Second}
+		_, err := proxy.Get(testURL)
+		if err == nil {
+			break
+		}
+	}
+	wg.Done()
+}
+
+// Get fetchs an URL with the given proxy and returns the body text
+func (proxy *Proxy) Get(url string) ([]byte, error) {
+	resp, err := proxy.client.Get(url)
+	if err != nil {
+		proxy.Alive = false
+		return nil, fmt.Errorf("HTTP request failed: %s", err)
+	}
+	defer resp.Body.Close()
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		proxy.Alive = false
+		return nil, fmt.Errorf("can't read response: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		proxy.Alive = false
+		return nil, fmt.Errorf("test URL replied with HTTP code %d", resp.StatusCode)
+	}
+
+	proxy.Alive = true
+	return buf, nil
+}
+
+// GetAvailableProxy returns an available proxy from the pool.
+func (pool *ProxyPool) GetAvailableProxy() (*Proxy, error) {
+	for i := range pool.proxies {
+		if time.Since(pool.proxies[i].lastRequest).Seconds() > float64(pool.RateLimit) && pool.proxies[i].Alive {
+			return pool.proxies[i], nil
+		}
+	}
+	return nil, errors.New("no proxies available")
+}
+
+// NewProxyPool constructs a new ProxyPool instance
+func NewProxyPool(testURL string, rateLimit int, retestDelay int) *ProxyPool {
+	pool := &ProxyPool{TestURL: testURL, RateLimit: rateLimit, RetestDelay: retestDelay}
+	return pool
+}
+
+// LoadProxies loads the pool with SOCKS4/5 proxies from a text file
+func (pool *ProxyPool) LoadProxies(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var wg sync.WaitGroup
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ":")
+		ip := line[0]
+		port, _ := strconv.Atoi(line[1])
+		proxy := Proxy{ip: ip, port: port}
+		pool.proxies = append(pool.proxies, &proxy)
+
+		wg.Add(1)
+		go proxy.Create(pool.TestURL, &wg)
+	}
+	wg.Wait()
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	pool.updateCounts()
+	return nil
+}
+
+// Get waits until a proxy from the pool is available and then fetchs the given URL.
+func (pool *ProxyPool) Get(url string) []byte {
+	for {
+		proxy, err := pool.GetAvailableProxy()
+		if err == nil {
+			r, reqErr := proxy.Get(url)
+			if reqErr == nil {
+				pool.updateCounts()
+				return r
+			}
+		}
+	}
+}
+
+// Test tests all proxies in the pool that are unavailable and their last request time is older than the Pool's RetestDelay
+func (pool *ProxyPool) Test() {
+	for i := range pool.proxies {
+		if time.Since(pool.proxies[i].lastRequest).Seconds() > float64(pool.RetestDelay) && !pool.proxies[i].Alive {
+			pool.proxies[i].Get(pool.TestURL)
+		}
+	}
+}
+
+// updateCounts updates the pool counters
+func (pool *ProxyPool) updateCounts() {
+	var alive, available int
+	for i := range pool.proxies {
+		if pool.proxies[i].Alive {
+			alive++
+			if time.Since(pool.proxies[i].lastRequest).Seconds() > float64(pool.RateLimit) {
+				available++
+			}
+		}
+	}
+	pool.AliveCount = alive
+	pool.AvailableCount = available
+}
