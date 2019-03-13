2d2096a515242d49ea754f68d7b1fc8b5619b541
Tik <tik@lag.party>
2019-03-13 08:32:32 -0300

    Disable keep-alives

--- proxypool.go
+++ proxypool.go
@@ -49,9 +49,8 @@ func (proxy *Proxy) Create(testURL string, wg *sync.WaitGroup) {
 		proxy.URL = fmt.Sprintf("socks%d://%s:%d", version, proxy.ip, proxy.port)
 		proxy.dial = socks.Dial(proxy.URL + "?timeout=10s")
 		proxy.transport = &http.Transport{
-			Dial:            proxy.dial,
-			MaxIdleConns:    10,
-			IdleConnTimeout: 30 * time.Second,
+			Dial:              proxy.dial,
+			DisableKeepAlives: true,
 		}
 		proxy.client = &http.Client{Transport: proxy.transport, Timeout: 10 * time.Second}
 		_, err := proxy.Get(testURL)
